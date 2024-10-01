d599a75297c2cd5425f12de99fe3c7b6065f2daf
Ilya Grig <ilia.grigoriev2018@yandex.ru>
2024-10-01 21:24:40 +0400

    good

--- main/urls.py
+++ main/urls.py
@@ -8,9 +8,5 @@ app_name = 'main'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
 ]
-
-# urlpatterns = [
-#     path('', views.IndexView.as_view(), name='index'),
-#     path('about/', cache_page(60)(views.AboutView.as_view()), name='about'),
-# ]
--- main/views.py
+++ main/views.py
@@ -22,7 +22,25 @@ class AboutView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Home - О нас'
         context['content'] = "О нас"
-        context['text_on_page'] = "здесь будет инфа о мед центре"
+        context['text_on_page'] = ("Самарский диагностический центр начал свою работу в 1990 году. В центре работают "
+                                   "отделы: консультативно-поликлинический, рентгеновской лучевой диагностики, "
+                                   "ультразвуковой диагностики, лабораторной диагностики, функциональных "
+                                   "исследований, гинекологии, оперативных вмешательств, эндоскопический кабинет. "
+                                   "Также активно развивается педиатрическое направление. Есть физиотерапевтическое "
+                                   "лечение, дневной стационар.")
         return context
 
 
+class ContactView(TemplateView):
+    template_name = 'main/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['title'] = 'Home - О нас'
+        context['content'] = "Контактная информация "
+        context['text_on_page'] = ("тел: 8 (846) 300-44-63,"
+                                   "8 (846) 260-61-53,\n"
+                                   "местонахождение: г.Самара, ул. Мяги, 7а,\n"
+                                   "email: samara_dc@mail.ru")
+
+        return context
